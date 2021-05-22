9ff08293f821ecdeac1c6b0af70fc6d2794dae4b
Attila Szabo <attila.szabo@physics.ox.ac.uk>
2021-05-22 21:26:09 +0100

    check jit gains in benchmarks

--- Benchmarks/qgt.py
+++ Benchmarks/qgt.py
@@ -20,7 +20,16 @@ class FFNN(nn.Module):
     bias_init: NNInitFunc = normal(0.01)
 
     def setup(self):
-        self.layers = [nk.nn.Dense(features = self.width, use_bias = True, dtype=self.dtype, kernel_init=self.kernel_init, bias_init=self.bias_init) for layer in range(self.n_layers)]
+        self.layers = [
+            nk.nn.Dense(
+                features=self.width,
+                use_bias=True,
+                dtype=self.dtype,
+                kernel_init=self.kernel_init,
+                bias_init=self.bias_init,
+            )
+            for layer in range(self.n_layers)
+        ]
 
     @nn.compact
     def __call__(self, x_in):
@@ -28,7 +37,7 @@ class FFNN(nn.Module):
         for layer in self.layers:
             x = layer(x)
             x = self.activation(x)
-        return jnp.sum(x,axis=-1) / (x.shape[-1])**0.5
+        return jnp.sum(x, axis=-1) / (x.shape[-1]) ** 0.5
 
 
 # Benchmark starts here
@@ -38,24 +47,24 @@ key_sample, key_rhs = jax.random.split(jax.random.PRNGKey(0), 2)
 
 graph = nk.graph.Chain(n_nodes)
 hilbert = nk.hilbert.Spin(s=0.5, N=n_nodes)
-machine = FFNN(n_layers = 8, width = 500)
+machine = FFNN(n_layers=8, width=500)
 
 # Create a variational state to run QGT on
 n_samples = 2000
-sa = nk.sampler.MetropolisExchange(hilbert = hilbert, graph=graph, d_max=2)
+sa = nk.sampler.MetropolisExchange(hilbert=hilbert, graph=graph, d_max=2)
 vstate = nk.variational.MCState(sampler=sa, model=machine, n_samples=n_samples)
-vstate.init(seed = 0)
+vstate.init(seed=0)
 # We don't actually want to perform a rather slow sampling
-vstate._samples = hilbert.random_state(key = key_sample, size = n_samples)
+vstate._samples = hilbert.random_state(key=key_sample, size=n_samples)
 
-qgt = qgt.QGTOnTheFly(vstate = vstate, diag_shift=0.01)
+qgt = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01)
 
 # Generate a random RHS of the same pytree shape as the parameters
 vec, unravel = nk.jax.tree_ravel(vstate.parameters)
-vec = jax.random.normal(key_rhs, shape = vec.shape, dtype = vec.dtype)
+vec = jax.random.normal(key_rhs, shape=vec.shape, dtype=vec.dtype)
 rhs = unravel(vec)
 
 start = datetime.now()
 lhs = qgt.solve(cg, rhs)
 end = datetime.now()
-print(f'took {(end-start).total_seconds()} seconds')
+print(f"took {(end-start).total_seconds()} seconds")
--- Benchmarks/qgt_gcnn.py
+++ Benchmarks/qgt_gcnn.py
@@ -1,5 +1,6 @@
 import netket as nk
 from netket.optimizer import qgt
+from flax.core import unfreeze
 from flax import linen as nn
 from flax.linen.initializers import normal, variance_scaling
 import jax
@@ -13,37 +14,59 @@ from datetime import datetime
 # Benchmark starts here
 
 side = 10
-n_nodes = side*side
-key_sample, key_rhs = jax.random.split(jax.random.PRNGKey(0), 2)
+n_nodes = side * side
+keys = jax.random.split(jax.random.PRNGKey(0), 5)
 
 graph = nk.graph.Square(side)
 hilbert = nk.hilbert.Spin(s=0.5, N=n_nodes)
 symm_group = graph.automorphisms()
 
-pure_jax = True # removes every remembrance of `symm_group` from the GCNN
+pure_jax = True  # removes every remembrance of `symm_group` from the GCNN
 if pure_jax:
-    symm = nk.utils.HashableArray(jnp.asarray(symm_group))
-    pt = nk.utils.HashableArray(jnp.asarray(symm_group.product_table.ravel()))
-    machine = nk.models.GCNN(symmetries = symm, flattened_product_table=pt, layers=8, features=5, bias_init=normal(0.01))
+    symm = nk.utils.HashableArray(np.asarray(symm_group))
+    pt = nk.utils.HashableArray(np.asarray(symm_group.product_table.ravel()))
+    machine = nk.models.GCNN(
+        symmetries=symm,
+        flattened_product_table=pt,
+        layers=8,
+        features=10,
+        bias_init=normal(0.01),
+    )
 else:
-    machine = nk.models.GCNN(symmetries = symm_group, layers=8, features=5, bias_init=normal(0.01))
+    machine = nk.models.GCNN(
+        symmetries=symm_group, layers=8, features=10, bias_init=normal(0.01)
+    )
 
 # Create a variational state to run QGT on
-n_samples = 2000
-sa = nk.sampler.MetropolisExchange(hilbert = hilbert, graph=graph, d_max=2)
+n_samples = 20000
+sa = nk.sampler.MetropolisExchange(hilbert=hilbert, graph=graph, d_max=2)
 vstate = nk.variational.MCState(sampler=sa, model=machine, n_samples=n_samples)
-vstate.init(seed = 0)
+vstate.init(seed=0)
 # We don't actually want to perform a rather slow sampling
-vstate._samples = hilbert.random_state(key = key_sample, size = n_samples)
+vstate._samples = hilbert.random_state(key=keys[0], size=n_samples)
 
-qgt = qgt.QGTOnTheFly(vstate = vstate, diag_shift=0.01)
+qgt_ = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01)
 
 # Generate a random RHS of the same pytree shape as the parameters
 vec, unravel = nk.jax.tree_ravel(vstate.parameters)
-vec = jax.random.normal(key_rhs, shape = vec.shape, dtype = vec.dtype)
-rhs = unravel(vec)
+vec1 = jax.random.normal(keys[1], shape=vec.shape, dtype=vec.dtype)
+rhs1 = unravel(vec1)
+vec2 = jax.random.normal(keys[2], shape=vec.shape, dtype=vec.dtype)
+rhs2 = unravel(vec2)
+vecp = vec * jax.random.normal(keys[3], shape=vec.shape, dtype=vec.dtype)
+pars = unravel(vecp)
 
 start = datetime.now()
-lhs = qgt.solve(cg, rhs)
+lhs = qgt_.solve(cg, rhs1)
 end = datetime.now()
-print(f'took {(end-start).total_seconds()} seconds')
+print(f"First time took {(end-start).total_seconds()} seconds")
+
+# See what jit hath wrought us
+vstate._samples = hilbert.random_state(key=keys[4], size=n_samples)
+vstate._parameters = pars
+qgt_ = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01)
+
+start = datetime.now()
+lhs = qgt_.solve(cg, rhs2)
+end = datetime.now()
+print(f"Second time took {(end-start).total_seconds()} seconds")
