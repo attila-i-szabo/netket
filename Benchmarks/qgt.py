12bfe9599a149cd279b33414741900832aa95ccf
Attila Szabó <attila.szabo@physics.ox.ac.uk>
2021-06-19 16:17:45 +0200

    more detailed dense benchmarks

--- Benchmarks/qgt.py
+++ Benchmarks/qgt.py
@@ -9,6 +9,7 @@ import numpy as np
 from typing import Any
 from netket.utils.types import NNInitFunc
 from datetime import datetime
+from timeit import timeit
 
 
 class FFNN(nn.Module):
@@ -41,44 +42,59 @@ class FFNN(nn.Module):
 
 
 # Benchmark starts here
+def benchmark(n_nodes, n_samples, n_layers, width):
+    print(f'Dense network\n* {n_nodes} nodes\n* {n_samples} samples')
+    print(f'* {n_layers} dense layers with {width} hidden units')
+    keys = jax.random.split(jax.random.PRNGKey(0), 5)
 
-n_nodes = 100
-keys = jax.random.split(jax.random.PRNGKey(0), 5)
+    graph = nk.graph.Chain(n_nodes)
+    hilbert = nk.hilbert.Spin(s=0.5, N=n_nodes)
+    machine = FFNN(n_layers=n_layers, width=width)
 
-graph = nk.graph.Chain(n_nodes)
-hilbert = nk.hilbert.Spin(s=0.5, N=n_nodes)
-machine = FFNN(n_layers=8, width=500)
+    # Create a variational state to run QGT on
+    sa = nk.sampler.MetropolisExchange(hilbert=hilbert, graph=graph, d_max=2)
+    vstate = nk.variational.MCState(sampler=sa, model=machine, n_samples=n_samples)
+    vstate.init(seed=0)
+    # We don't actually want to perform a rather slow sampling
+    vstate._samples = hilbert.random_state(key=keys[0], size=n_samples)
 
-# Create a variational state to run QGT on
-n_samples = 2000
-sa = nk.sampler.MetropolisExchange(hilbert=hilbert, graph=graph, d_max=2)
-vstate = nk.variational.MCState(sampler=sa, model=machine, n_samples=n_samples)
-vstate.init(seed=0)
-# We don't actually want to perform a rather slow sampling
-vstate._samples = hilbert.random_state(key=keys[0], size=n_samples)
+    qgt_ = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01, centered=False)
 
-qgt_ = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01, centered=False)
+    # Generate a random RHS of the same pytree shape as the parameters
+    vec, unravel = nk.jax.tree_ravel(vstate.parameters)
+    vec1 = jax.random.normal(keys[1], shape=vec.shape, dtype=vec.dtype)
+    rhs1 = unravel(vec1)
+    vec2 = jax.random.normal(keys[2], shape=vec.shape, dtype=vec.dtype)
+    rhs2 = unravel(vec2)
+    # Generate a new set of parameters for the second set of runs
+    vecp = vec * jax.random.normal(keys[3], shape=vec.shape, dtype=vec.dtype)
+    pars = unravel(vecp)
 
-# Generate a random RHS of the same pytree shape as the parameters
-vec, unravel = nk.jax.tree_ravel(vstate.parameters)
-vec1 = jax.random.normal(keys[1], shape=vec.shape, dtype=vec.dtype)
-rhs1 = unravel(vec1)
-vec2 = jax.random.normal(keys[2], shape=vec.shape, dtype=vec.dtype)
-rhs2 = unravel(vec2)
-vecp = vec * jax.random.normal(keys[3], shape=vec.shape, dtype=vec.dtype)
-pars = unravel(vecp)
+    time = timeit(lambda: jax.tree_map(lambda x: x.block_until_ready(), qgt_.solve(cg, rhs1)), number=1)
+    print(f"* Jitting run took {time} seconds")
 
-start = datetime.now()
-lhs = jax.tree_map(lambda x: x.block_until_ready(), qgt_.solve(cg, rhs1))
-end = datetime.now()
-print(f"First time took {(end-start).total_seconds()} seconds")
+    # See what jit hath wrought us
+    vstate._samples = hilbert.random_state(key=keys[4], size=n_samples)
+    vstate._parameters = pars
+    qgt_ = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01, centered=False)
 
-# See what jit hath wrought us
-vstate._samples = hilbert.random_state(key=keys[4], size=n_samples)
-vstate._parameters = pars
-qgt_ = qgt.QGTOnTheFly(vstate=vstate, diag_shift=0.01, centered=False)
+    time = timeit(lambda: jax.tree_map(lambda x: x.block_until_ready(), qgt_.solve(cg, rhs2)), number = 10)/10
+    print(f"* After jitting, average runtime is {time} seconds\n")
 
-start = datetime.now()
-lhs = jax.tree_map(lambda x: x.block_until_ready(), qgt_.solve(cg, rhs2))
-end = datetime.now()
-print(f"Second time took {(end-start).total_seconds()} seconds")
+print('## Different network widths/system sizes')
+benchmark(64, 128, 4, 64)
+benchmark(128, 128, 4, 128)
+benchmark(256, 128, 4, 256)
+benchmark(512, 128, 4, 512)
+
+print('## Different sample numbers')
+benchmark(128, 128, 4, 128)
+benchmark(128, 256, 4, 128)
+benchmark(128, 512, 4, 128)
+benchmark(128, 1024, 4, 128)
+
+print('## Different number of layers')
+benchmark(128, 128, 4, 128)
+benchmark(128, 128, 6, 128)
+benchmark(128, 128, 8, 128)
+benchmark(128, 128, 12, 128)
